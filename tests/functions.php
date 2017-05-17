9f092e46cb9071c4901d9efccca9729813528967
svitcode <yarik.proger@gmail.com>
2017-05-17 10:27:30 +0300

    .

--- tests/functions.php
+++ tests/functions.php
@@ -10,13 +10,13 @@
  */
 
 function testRemoveDirTree($dir)
-{ 
-    if(!file_exists($dir)) {
+{
+    if (!file_exists($dir)) {
         return;
     }
-    $files = array_diff(scandir($dir), array('.','..')); 
-    foreach ($files as $file) { 
-        (is_dir("$dir/$file")) ? testRemoveDirTree("$dir/$file") : unlink("$dir/$file"); 
-    } 
-    return rmdir($dir); 
+    $files = array_diff(scandir($dir), ['.','..']);
+    foreach ($files as $file) {
+        (is_dir("$dir/$file")) ? testRemoveDirTree("$dir/$file") : unlink("$dir/$file");
+    }
+    return rmdir($dir);
 }
