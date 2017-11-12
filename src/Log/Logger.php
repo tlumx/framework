dc344e764e1aa451bea73496f7303ff7e20a4933
svitcode <yarik.proger@gmail.com>
2017-11-12 22:50:40 +0200

    Creating the Log component.

---
+++ src/Log/Logger.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Tlumx Framework (https://framework.tlumx.xyz/)
+ *
+ * @author    Yaroslav Kharitonchuk <yarik.proger@gmail.com>
+ * @link      https://github.com/tlumx/framework
+ * @copyright Copyright (c) 2016-2017 Yaroslav Kharitonchuk
+ * @license   https://framework.tlumx.xyz/license  (MIT License)
+ */
+namespace Tlumx\Log;
+
+class Logger
+{
+    
+}
