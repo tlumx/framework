4ae2bb32f48f6239c90eebc5e9377d012e58b250
svitcode <yarik.proger@gmail.com>
2017-11-13 15:18:57 +0200

    Add Translation component

---
+++ src/Translation/Loader/Po.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Tlumx Framework (https://framework.tlumx.xyz/)
+ *
+ * @author    Yaroslav Kharitonchuk <yarik.proger@gmail.com>
+ * @link      https://github.com/tlumx/framework
+ * @copyright Copyright (c) 2016-2017 Yaroslav Kharitonchuk
+ * @license   https://framework.tlumx.xyz/license  (MIT License)
+ */
+namespace Tlumx\Translation\Loader;
+
+/**
+ * Po translation loader class.
+ */
+class Po implements LoaderInterface
+{
+    /**
+     * Loads messages from an PO file
+     *
+     * @param string $filename
+     * @return array
+     * @throws \InvalidArgumentException
+     */
+    public function load($filename)
+    {
+        if (!is_file($filename) || !is_readable($filename)) {
+            throw new \InvalidArgumentException(sprintf('Could not open file %s for reading', $filename));
+        }
+
+        $content = implode('', file($filename));
+        $count = preg_match_all(
+            '/msgid\s+((?:".*(?<!\\\\)"\s*)+)\s+' .
+                        'msgstr\s+((?:".*(?<!\\\\)"\s*)+)/',
+            $content,
+            $matches
+        );
+        $messages = [];
+        for ($i = 0; $i < $count; $i++) {
+            $key = substr(rtrim($matches[1][$i]), 1, -1);
+            $value = substr(rtrim($matches[2][$i]), 1, -1);
+            if ($key) {
+                $messages[$key] = $value;
+            }
+        }
+
+        return $messages;
+    }
+}
