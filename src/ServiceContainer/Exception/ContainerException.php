7a9643ee0b1a325167ad09f5789682ff8afb246f
svitcode <yarik.proger@gmail.com>
2017-11-13 11:44:29 +0200

    Create Service container

---
+++ src/ServiceContainer/Exception/ContainerException.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Tlumx Framework (https://framework.tlumx.xyz/)
+ *
+ * @author    Yaroslav Kharitonchuk <yarik.proger@gmail.com>
+ * @link      https://github.com/tlumx/framework
+ * @copyright Copyright (c) 2016-2017 Yaroslav Kharitonchuk
+ * @license   https://framework.tlumx.xyz/license  (MIT License)
+ */
+namespace Tlumx\ServiceContainer\Exception;
+
+use Interop\Container\Exception\ContainerException as InteropContainerException;
+
+class ContainerException extends \RuntimeException implements InteropContainerException
+{
+}
