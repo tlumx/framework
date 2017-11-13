17e99d607ee4725424d676c508f6817df7d46b64
svitcode <yarik.proger@gmail.com>
2017-11-13 15:50:29 +0200

    fix error namespace

--- tests/Db/DbTest.php
+++ tests/Db/DbTest.php
@@ -7,7 +7,7 @@
  * @copyright Copyright (c) 2016-2017 Yaroslav Kharitonchuk
  * @license   https://framework.tlumx.xyz/license  (MIT License)
  */
-namespace Tlum\Tests\Db;
+namespace Tlumx\Tests\Db;
 
 use Tlumx\Db\Db;
 
