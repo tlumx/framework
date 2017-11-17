cd33987f41cc50248eae262ec625146cc41913f5
svitcode <yarik.proger@gmail.com>
2017-11-18 00:05:17 +0200

    add fixtures class need for testing

---
+++ tests/ServiceContainer/MyFactory.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Tlumx Framework (https://framework.tlumx.xyz/)
+ *
+ * @author    Yaroslav Kharitonchuk <yarik.proger@gmail.com>
+ * @link      https://github.com/tlumx/framework
+ * @copyright Copyright (c) 2016-2017 Yaroslav Kharitonchuk
+ * @license   https://framework.tlumx.xyz/license  (MIT License)
+ */
+namespace Tlumx\Tests\ServiceContainer;
+
+use Tlumx\ServiceContainer\ServiceContainer;
+
+class MyFactory
+{
+    public function __invoke(ServiceContainer $container)
+    {
+        return rand();
+    }
+}
