86b7f6ddbc9dd036ab329334ea6c9bcd47cfca51
svitcode <yarik.proger@gmail.com>
2017-11-18 00:07:26 +0200

    created ServiceContainer tests

--- tests/ServiceContainer/ServiceContainerTest.php
+++ tests/ServiceContainer/ServiceContainerTest.php
@@ -50,8 +50,8 @@ class ServiceContainerTest extends \PHPUnit_Framework_TestCase
             'service3' => 'value3'
         ]);
         $this->assertTrue($c->has('service1'));
-        $this->assertTrue($c->has('service1'));
-        $this->assertTrue($c->has('service1'));
+        $this->assertTrue($c->has('service2'));
+        $this->assertTrue($c->has('service3'));
         $this->assertEquals('value1', $c->get('service1'));
         $this->assertEquals('value2', $c->get('service2'));
         $this->assertEquals('value3', $c->get('service3'));
@@ -69,14 +69,7 @@ class ServiceContainerTest extends \PHPUnit_Framework_TestCase
         );
 
         $c = new ServiceContainer();
-        $c->get('some');
-    }
-
-    public function testRegisterSomeValue()
-    {
-        $c = new ServiceContainer();
-        $c->register('some', 'value');
-        $this->assertEquals('value', $c->get('some'));
+        $c->get($index);
     }
 
     public function testRegisterClosure()
@@ -93,13 +86,72 @@ class ServiceContainerTest extends \PHPUnit_Framework_TestCase
         $this->assertNotSame($c->get('service2'), $c->get('service2'));
     }
 
+    public function testInvalidRegisterFactoryNotCallable()
+    {
+        $c = new ServiceContainer();
+        $c->register('service1', 'Tlumx\Tests\ServiceContainer\A');
+        $this->setExpectedException(
+            'Tlumx\ServiceContainer\Exception\ContainerException',
+            'Service could not be created: "Service factory may be callable or string '
+                . '(that can be resolving to an invokable class '
+                . 'or to a FactoryInterface instance).'
+        );
+
+        $c->get('service1');
+    }
+
+    public function testInvalidRegisterFactoryNotInvokeClass()
+    {
+        $c = new ServiceContainer();
+        $c->register('service1', 'Tlumx\Tests\ServiceContainer\NotInvokable');
+        $this->setExpectedException(
+            'Tlumx\ServiceContainer\Exception\ContainerException',
+            'Service could not be created: "There were incorrectly '
+                . 'transmitted data when registering the service".'
+        );
+
+        $c->get('service1');
+    }
+
+    public function testInvalidRegisterFactoryNotStringClass()
+    {
+        $c = new ServiceContainer();
+        $c->register('service1', 'not_class');
+        $this->setExpectedException(
+            'Tlumx\ServiceContainer\Exception\ContainerException',
+            'Service could not be created: "There were incorrectly '
+                . 'transmitted data when registering the service".'
+        );
+
+        $c->get('service1');
+    }
+
+    public function testRegisterFactoryFromString()
+    {
+        $c = new ServiceContainer();
+        $c->register('service1', 'Tlumx\Tests\ServiceContainer\MyFactory');
+        $c->register('service2', 'Tlumx\Tests\ServiceContainer\MyFactory', false);
+
+        $this->assertSame($c->get('service1'), $c->get('service1'));
+        $this->assertNotSame($c->get('service2'), $c->get('service2'));
+
+        $c->register('service3', 'Tlumx\Tests\ServiceContainer\MyFactory2');
+        $c->register('service4', 'Tlumx\Tests\ServiceContainer\MyFactory2', false);
+
+        $this->assertEquals(1, $c->get('service3'));
+        $this->assertEquals(1, $c->get('service4'));
+        $c->set('a', 2);
+        $this->assertEquals(1, $c->get('service3'));
+        $this->assertEquals(3, $c->get('service4'));
+    }
+
     public function testRegisterImmutableService()
     {
         $index = 'some';
         $c = new ServiceContainer();
-        $c->register('some', function () {
+        $c->register($index, function () {
         });
-        $some = $c->get('some');
+        $val = $c->get($index);
         $this->setExpectedException(
             'Tlumx\ServiceContainer\Exception\ContainerException',
             sprintf(
@@ -107,7 +159,8 @@ class ServiceContainerTest extends \PHPUnit_Framework_TestCase
                 $index
             )
         );
-        $c->register('some', []);
+        $c->register($index, function () {
+        });
     }
 
     public function testSetIfImmutable()
@@ -115,7 +168,7 @@ class ServiceContainerTest extends \PHPUnit_Framework_TestCase
         $c = new ServiceContainer();
         $c->register('some', function () {
         });
-        $some = $c->get('some');
+        $val = $c->get('some');
         $this->setExpectedException(
             'Tlumx\ServiceContainer\Exception\ContainerException',
             sprintf(
@@ -127,12 +180,31 @@ class ServiceContainerTest extends \PHPUnit_Framework_TestCase
         $c->set('some', 'value');
     }
 
+    public function testRegisterFromDefinitionImmutableService()
+    {
+        $index = 'some';
+        $c = new ServiceContainer();
+        $c->register($index, function () {
+        });
+        $val = $c->get($index);
+        $this->setExpectedException(
+            'Tlumx\ServiceContainer\Exception\ContainerException',
+            sprintf(
+                'A service by the name "%s" already exists and cannot be overridden',
+                $index
+            )
+        );
+        $c->registerDefinition($index, []);
+    }
+
     public function testRegisterFromDefinitionErrorClassNotDefined()
     {
         $c = new ServiceContainer();
-        $c->register('service1', []);
+        $c->registerDefinition('service1', []);
         $this->setExpectedException(
-            'Tlumx\ServiceContainer\Exception\ContainerException'
+            'Tlumx\ServiceContainer\Exception\ContainerException',
+            'Service could not be created from definition:'
+                . ' option "class" is not exists in definition array.'
         );
         $c->get('service1');
     }
@@ -140,12 +212,13 @@ class ServiceContainerTest extends \PHPUnit_Framework_TestCase
     public function testRegisterFromDefinitionErrorClassIsNotExists()
     {
         $c = new ServiceContainer();
-        $c->register('service1', [
+        $c->registerDefinition('service1', [
             'class' => 'SomeInvalidClass'
         ]);
         $this->setExpectedException(
             'Tlumx\ServiceContainer\Exception\ContainerException',
-            'Class "SomeInvalidClass" is not exists'
+            'Service could not be created from definition:'
+                . ' Class "SomeInvalidClass" is not exists.'
         );
         $c->get('service1');
     }
@@ -153,12 +226,13 @@ class ServiceContainerTest extends \PHPUnit_Framework_TestCase
     public function testRegisterFromDefinitionErrorClassIsNotInstantiable()
     {
         $c = new ServiceContainer();
-        $c->register('service1', [
+        $c->registerDefinition('service1', [
             'class' => 'Tlumx\Tests\ServiceContainer\NotInstantiable'
         ]);
         $this->setExpectedException(
             'Tlumx\ServiceContainer\Exception\ContainerException',
-            'Unable to create instance of class: "Tlumx\Tests\ServiceContainer\NotInstantiable"'
+            'Service could not be created from definition:'
+                . ' Unable to create instance of class "Tlumx\Tests\ServiceContainer\NotInstantiable".'
         );
         $c->get('service1');
     }
@@ -166,12 +240,13 @@ class ServiceContainerTest extends \PHPUnit_Framework_TestCase
     public function testRegisterFromDefinitionErrorConstructorArgsIsNotExists()
     {
         $c = new ServiceContainer();
-        $c->register('service1', [
+        $c->registerDefinition('service1', [
             'class' => 'Tlumx\Tests\ServiceContainer\A'
         ]);
         $this->setExpectedException(
             'Tlumx\ServiceContainer\Exception\ContainerException',
-            'Option "args" is not exists in definition array'
+            'Service could not be created from definition:'
+                . ' option "args" is not exists in definition array.'
         );
         $c->get('service1');
     }
@@ -179,7 +254,7 @@ class ServiceContainerTest extends \PHPUnit_Framework_TestCase
     public function testRegisterFromDefinitionErrorCanNotCallMethod()
     {
         $c = new ServiceContainer();
-        $c->register('service1', [
+        $c->registerDefinition('service1', [
             'class' => 'Tlumx\Tests\ServiceContainer\A',
             'args' => ['some value'],
             'calls' => [
@@ -188,7 +263,8 @@ class ServiceContainerTest extends \PHPUnit_Framework_TestCase
         ]);
         $this->setExpectedException(
             'Tlumx\ServiceContainer\Exception\ContainerException',
-            'Can not call method "invalidMethod" from class: "Tlumx\Tests\ServiceContainer\A"'
+            'Service could not be created from definition:'
+                . ' can not call method "invalidMethod" from class: "Tlumx\Tests\ServiceContainer\A"'
         );
         $c->get('service1');
     }
@@ -196,7 +272,7 @@ class ServiceContainerTest extends \PHPUnit_Framework_TestCase
     public function testRegisterFromDefinitionErrorInvalidArgs()
     {
         $c = new ServiceContainer();
-        $c->register('service1', [
+        $c->registerDefinition('service1', [
             'class' => 'Tlumx\Tests\ServiceContainer\A',
             'args' => ['some value'],
             'calls' => [
@@ -205,7 +281,8 @@ class ServiceContainerTest extends \PHPUnit_Framework_TestCase
         ]);
         $this->setExpectedException(
             'Tlumx\ServiceContainer\Exception\ContainerException',
-            'Unable resolve parameter'
+            'Service could not be created from definition:'
+                . ' unable resolve parameter.'
         );
         $c->get('service1');
     }
@@ -213,7 +290,7 @@ class ServiceContainerTest extends \PHPUnit_Framework_TestCase
     public function testRegisterFromDefinitionErrorArgsNotInContainer()
     {
         $c = new ServiceContainer();
-        $c->register('service1', [
+        $c->registerDefinition('service1', [
             'class' => 'Tlumx\Tests\ServiceContainer\A',
             'args' => ['some value'],
             'calls' => [
@@ -222,7 +299,8 @@ class ServiceContainerTest extends \PHPUnit_Framework_TestCase
         ]);
         $this->setExpectedException(
             'Tlumx\ServiceContainer\Exception\ContainerException',
-            'Unable resolve parameter'
+            'Service could not be created from definition:'
+                . ' unable resolve parameter.'
         );
         $c->get('service1');
     }
@@ -230,11 +308,11 @@ class ServiceContainerTest extends \PHPUnit_Framework_TestCase
     public function testRegisterFromDefinition()
     {
         $c = new ServiceContainer();
-        $c->register('B', [
+        $c->registerDefinition('B', [
             'class' => 'Tlumx\Tests\ServiceContainer\B',
             'args' => ['a' => ['ref' => 'A']]
         ]);
-        $c->register('A', [
+        $c->registerDefinition('A', [
             'class' => 'Tlumx\Tests\ServiceContainer\A',
             'args' => ['some value'],
             'calls' => [
@@ -265,26 +343,63 @@ class ServiceContainerTest extends \PHPUnit_Framework_TestCase
         $c->setAlias('some', 'some');
     }
 
+    public function testGetSetIssetUnsetAliases()
+    {
+        $c = new ServiceContainer();
+        $this->assertEquals([], $c->getAliases());
+        $c->setAlias('alias1', 'service1');
+        $c->setAlias('alias2', 'service1');
+        $this->assertEquals('service1', $c->getServiceIdFromAlias('alias1'));
+        $this->assertEquals('service1', $c->getServiceIdFromAlias('alias2'));
+        $this->assertEquals(null, $c->getServiceIdFromAlias('alias3'));
+        $this->assertEquals(false, $c->getServiceIdFromAlias('alias3', false));
+        $this->assertEquals(['alias1' => 'service1', 'alias2' => 'service1'], $c->getAliases());
+        $this->assertTrue($c->hasAlias('alias1'));
+        $this->assertTrue($c->hasAlias('alias2'));
+        $this->assertFalse($c->hasAlias('alias3'));
+        $c->removeAlias('alias1');
+        $this->assertEquals(['alias2' => 'service1'], $c->getAliases());
+    }
+
     public function testUseAlias()
     {
         $c = new ServiceContainer();
-        $c->register('B', [
+        $c->registerDefinition('B', [
             'class' => 'Tlumx\Tests\ServiceContainer\B',
             'args' => ['a' => ['ref' => 'A']]
         ]);
-        $c->register('A', [
+        $c->registerDefinition('A', [
             'class' => 'Tlumx\Tests\ServiceContainer\A',
             'args' => ['some value'],
             'calls' => [
                 'setContainer' => [ ['ref' => 'this'] ]
             ]
         ]);
+        $c->register('C', function () {
+        });
         $c->set('a', 'aaa');
         $c->setAlias('a-alias', 'a');
         $c->setAlias('B-alias', 'B');
+        $c->setAlias('c_alias', 'C');
 
+        $this->assertTrue($c->hasAlias('a-alias'));
+        $this->assertTrue($c->hasAlias('B-alias'));
+        $this->assertTrue($c->hasAlias('c_alias'));
         $this->assertEquals($c->get('a'), $c->get('a-alias'));
         $this->assertNotSame($c->get('A'), $c->get('B-alias'));
         $this->assertSame($c->get('B'), $c->get('B-alias'));
+
+        // if service name overide alias name, alias remove
+        $c->set('a-alias', 'some value');
+        $this->assertNotEquals($c->get('a'), $c->get('a-alias'));
+        $this->assertFalse($c->hasAlias('a-alias'));
+        $c->registerDefinition('B-alias', [
+            'class' => 'Tlumx\Tests\ServiceContainer\B',
+            'args' => ['a' => ['ref' => 'A']]
+        ]);
+        $this->assertFalse($c->hasAlias('B-alias'));
+        $c->register('c_alias', function () {
+        });
+        $this->assertFalse($c->hasAlias('c_alias'));
     }
 }
