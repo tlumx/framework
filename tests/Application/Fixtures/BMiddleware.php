c32a2e48048975a5ca5873bd4d952fa64015cb7c
Yaroslav Kharitonchuk <yarik.proger@gmail.com>
2018-08-02 19:32:40 +0300

    Add fixture Middleware for testing

---
+++ tests/Application/Fixtures/BMiddleware.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Tlumx (https://tlumx.com/)
+ *
+ * @author    Yaroslav Kharitonchuk <yarik.proger@gmail.com>
+ * @link      https://github.com/tlumx/framework
+ * @copyright Copyright (c) 2016-2018 Yaroslav Kharitonchuk
+ * @license   https://github.com/tlumx/framework/blob/master/LICENSE.md  (MIT License)
+ */
+namespace Tlumx\Tests\Application\Fixtures;
+
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\ResponseInterface;
+
+class BMiddleware implements MiddlewareInterface
+{
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+
+        $msg = $request->getAttribute('msg');
+
+        return $handler->handle($request->withAttribute('msg', 'from_b_midds'));
+    }
+}
