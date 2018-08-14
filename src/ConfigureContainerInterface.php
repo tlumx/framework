b70020d8799082d673492693b48d3eee1e3020b3
Yaroslav Kharitonchuk <yarik.proger@gmail.com>
2018-08-14 22:23:44 +0300

    Add interface for configure Psr container

---
+++ src/ConfigureContainerInterface.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Tlumx (https://tlumx.com/)
+ *
+ * @author    Yaroslav Kharitonchuk <yarik.proger@gmail.com>
+ * @link      https://github.com/tlumx/framework
+ * @copyright Copyright (c) 2016-2018 Yaroslav Kharitonchuk
+ * @license   https://github.com/tlumx/framework/blob/master/LICENSE.md  (MIT License)
+ */
+namespace Tlumx\Application;
+
+use Psr\Container\ContainerInterface;
+
+/**
+* Interface for configuration Psr\Container\ContainerInterface
+*/
+interface ConfigureContainerInterface
+{
+    /**
+    * Configure given Psr\Container\ContainerInterface
+    *
+    * @param Psr\Container\ContainerInterface $container
+    * @param array $config
+    * @return void
+    */
+    public function configureContainer(ContainerInterface $container, array $config = []) : void;
+}
