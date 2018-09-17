9c318c8b82c126edfdec3471d59cf9e59a9ba899
Yaroslav Kharitonchuk <yarik.proger@gmail.com>
2018-09-17 18:09:31 +0300

    update

--- src/Handler/DefaultExceptionHandler.php
+++ src/Handler/DefaultExceptionHandler.php
@@ -9,7 +9,8 @@
  */
 namespace Tlumx\Application\Handler;
 
-use Tlumx\Application\ServiceProvider;
+use Psr\Container\ContainerInterface;
+use Psr\Http\Message\ResponseInterface;
 
 /**
  * Default exception handler.
@@ -17,18 +18,18 @@ use Tlumx\Application\ServiceProvider;
 class DefaultExceptionHandler implements ExceptionHandlerInterface
 {
     /**
-     * @var \Tlumx\ServiceProvider
+     * @var Psr\Container\ContainerInterface
      */
-    protected $provider;
+    protected $container;
 
     /**
      * Constructor
      *
-     * @param ServiceProvider $provider
+     * @param ContainerInterface $container
      */
-    public function __construct(ServiceProvider $provider)
+    public function __construct(ContainerInterface $container)
     {
-        $this->provider = $provider;
+        $this->container = $container;
     }
 
     /**
@@ -37,29 +38,41 @@ class DefaultExceptionHandler implements ExceptionHandlerInterface
      * @param \Exception $e
      * @return \Psr\Http\Message\ResponseInterface
      */
-    public function handle(\Exception $e)
+    public function handle(\Exception $e): ResponseInterface
     {
-        $response = $this->provider->getResponse();
+        $response = $this->container->get('response');
         $response = $response->withStatus(500);
         $message = 'Internal Server Error';
 
-        $view = $this->provider->getView();
-        if ($this->provider->getConfig('display_exceptions')) {
+        $view = $this->container->get('view');
+        if ($this->container->get('config')->get('display_exceptions')) {
             $view->exception = $e;
         }
         $view->message = $message;
 
-        $config = $this->provider->getConfig();
+        $config = $this->container->get('config');
         if (isset($config['templates']['template_error'])) {
             $result = $view->renderFile($config['templates']['template_error']);
         } else {
             if (isset($view->exception)) {
-                $body = sprintf("<h1>An error occurred</h1><h2>%s</h2><h3>Message</h3><p>%s</p><h3>Stack trace</h3><p>%s</p>", $message, $e->getMessage(), $e->getTraceAsString());
+                $body = sprintf(
+                    "<h1>An error occurred</h1><h2>%s</h2><h3>Message</h3>".
+                    "<p>%s</p><h3>Stack trace</h3><p>%s</p>",
+                    $message,
+                    $e->getMessage(),
+                    $e->getTraceAsString()
+                );
             } else {
                 $body = sprintf("<h1>An error occurred</h1><h2>%s</h2>", $message);
             }
 
-            $result = sprintf("<html><head><title>%s</title><style>body {font-family: Helvetica,Arial,sans-serif;font-size: 20px;line-height: 28px;padding:20px;}</style></head><body>%s</body></html>", 'Tlumx application: '.$message, $body);
+            $result = sprintf(
+                "<html><head><title>%s</title>".
+                "<style>body{font-family:Helvetica,Arial,sans-serif;".
+                "font-size:20px;line-height:28px;padding:20px;}</style></head><body>%s</body></html>",
+                'Tlumx application: '. $message,
+                $body
+            );
         }
 
         $response->getBody()->write($result);
